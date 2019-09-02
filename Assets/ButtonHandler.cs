39439f15e9b8ac812ba147c9cc4487e870c1741b
Emiliano Perez <emiliano.perez@etermax.com>
2019-09-02 14:12:54 -0300

    initial commit

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    [Header("Setup")]
+    public HttpRequester httpRequester;
+    public TextMeshProUGUI result;
+    
+    [Header("GET")]
+    public string urlGet;
+    public Button getButton;
+    public Button.ButtonClickedEvent getButtonEvent;
+
+    [Header("POST")]
+    public string urlPost;
+    public Button postButton;
+    public string postJsonParams;
+    public Button.ButtonClickedEvent postButtonEvent;
+
+    private void Start()
+    {
+        getButton.onClick = getButtonEvent;
+        postButton.onClick = postButtonEvent;
+    }
+
+    public void GetRequest()
+    {
+        httpRequester.GetJson(urlGet, UpdateResultText);
+    }
+
+    public void PostRequest()
+    {
+        httpRequester.PostJson(urlPost, postJsonParams, UpdateResultText);
+    }
+
+    private void UpdateResultText(string requestResponse)
+    {
+        result.text = requestResponse;
+    }
+}
---
+++ Assets/HttpRequester.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HttpRequester : MonoBehaviour
+{
+    public void GetJson(string url, Action<string> callback)
+    {
+        StartCoroutine(UnityGetJson(url, callback));
+    }
+
+    public void PostJson(string url, string jsonParams, Action<string> callback)
+    {
+        StartCoroutine(UnityPostJson(url, jsonParams, callback));
+    }
+
+    private IEnumerator UnityGetJson(string url, Action<string> callback)
+    {
+        UnityWebRequest uwr = UnityWebRequest.Get(url);
+        uwr.SetRequestHeader("Content-Type", "application/json");
+        uwr.SetRequestHeader("Accept", "application/json");
+        yield return uwr.SendWebRequest();
+
+        while (!uwr.downloadHandler.isDone)
+            yield return new WaitForEndOfFrame();
+
+        callback(uwr.isNetworkError ? uwr.error : uwr.downloadHandler.text);
+        yield break;
+    }
+    
+    private IEnumerator UnityPostJson(string url, string jsonParams, Action<string> callback)
+    {
+        UnityWebRequest uwr = UnityWebRequest.Post(url, "");
+        byte[] paramBytes = new System.Text.UTF8Encoding().GetBytes(jsonParams);
+        uwr.uploadHandler = new UploadHandlerRaw(paramBytes);
+        uwr.downloadHandler = new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
+        uwr.SetRequestHeader("Accept", "application/json");
+        yield return uwr.SendWebRequest();
+
+        while (!uwr.downloadHandler.isDone)
+            yield return new WaitForEndOfFrame();
+
+        callback(uwr.isNetworkError ? uwr.error : uwr.downloadHandler.text);
+        yield break;
+    }
+}
